c3955aa79e6f5954aa830b4cf0a34d67f73803a8
Ashok-2017 <84613826+Ashok-2017@users.noreply.github.com>
2023-11-07 17:27:52 +0800

    Update aws_sso.tf

--- aws_sso.tf
+++ aws_sso.tf
@@ -1,1 +1,38 @@
+provider "aws" {
+  sso_start_url = "https://sso-url"
+  sso_account_id = "aws_id"
+  sso_region = "aws_region"
+  sso_role_name = "aws-sso-role-name"
+  sso_role_duration = 60s
+}
+
+resource "aws_ssoadmin_account_assignment" "rdp" {
+  name = "sso-instance"
+  instance_arn = "arn:aws:ec2:<REGION>:<ACCOUNT_ID>:instance/<INSTANCE_ID>"
+  target_id = "arn:aws:organizations::<ORGANIZATION_ID>:sso-instance-sso/sso-instance"
+  target_type = "aws_instance_access"
+  permission_set_arn = "arn:aws:sso:::permissionSet:/<PERMISSION_SET_ID>"
+}
+
+resource "aws_ssoadmin_permission_set" "rdp" {
+  name = "sso-instance"
+  session_duration = "PT1H"
+}
+
+resource "aws_ec2_instance" "rdp" {
+  ami = " "
+  instance_type = " "
+  key_pair =  " "
+}
+
+resource "aws_security_group" "rdp" {
+  name = "windows-rdp"
+
+  ingress {
+    from_port = 3389
+    to_port = 3389
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+
 
