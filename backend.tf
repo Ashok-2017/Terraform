3a83a99ddd276bae52b261565c20eff00ccbbf2c
Ashok-2017 <84613826+Ashok-2017@users.noreply.github.com>
2023-11-07 14:38:17 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,11 @@
-
+terraform {
+  backend "s3" {
+   bucket        = "ashok_test"
+    key           = "api_gateway/ashok/terraform.tfstate"
+    role_arn      = "arn:aws:iam:::role/devops"
+    session_name  = "devops_test"
+    region        = "us-east-1"
+    profile       = "infra"
+  }
+}
+    
